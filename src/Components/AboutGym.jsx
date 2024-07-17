e739eb3d3619b37ff9c29b7f355cfc218a40ed68
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-07-18 02:09:30 +0530

    About gym section added

---
+++ src/Components/AboutGym.jsx
@@ -0,0 +1,42 @@
+import {
+  ABOUT_CONTENT_PARA1,
+  ABOUT_CONTENT_PARA2,
+  ABOUT_HEADING,
+  ATHELETE_URL,
+} from "../utils/constants";
+import { motion } from "framer-motion";
+
+const AboutGym = () => {
+  return (
+    <div className="bg-purple-50">
+      <div className="flex mt-28 ">
+        <img
+          className="w-[35%] rounded-3xl ml-40 mb-40 mr-14 hover:scale-105 transition duration-500 ease-in-out"
+          src={ATHELETE_URL}
+          alt=""
+        />
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="w-[35%] flex flex-col justify-start"
+        >
+          <h1 className="text-[3.5rem] mt-14 tracking-wider text-[#ff0034] arsenal-sc-regular">
+            <span className="text-7xl">A</span>bout{" "}
+            <span className="text-7xl">O</span>ur{" "}
+            <span className="text-7xl">G</span>ym
+          </h1>
+          <h2 className="mt-0 font-bold text-xl">{ABOUT_HEADING}</h2>
+          <p className="mt-8 text-gray-500 text-lg">{ABOUT_CONTENT_PARA1}</p>
+          <p className="mt-3 text-gray-500 text-lg">{ABOUT_CONTENT_PARA2}</p>
+          <button className="btn arsenal-sc-regular contact-hover w-2/6 rounded-full text-white tracking-wider p-3 pl-6 pr-6 mt-6">
+            Take a Tour &rarr;
+          </button>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutGym;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,16 @@ const WORKOUT_URL =
 const NUTRITION_URL =
   "https://gym.peushjha.com/wp-content/uploads/2023/11/shape-52.png";
 
+export const ATHELETE_URL =
+  "https://images.pexels.com/photos/1431282/pexels-photo-1431282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+export const ABOUT_HEADING = "We Can Give A Shape Of Your Body Here!";
+export const ABOUT_CONTENT_PARA1 =
+  "In Latest fitness equipment. 4,000 sq.ft. of gym workout space. A brand new gym. Dedicated personal training and CrossFit Studio. Internationally certified personal trainers. We are the champions of fitness";
+
+export const ABOUT_CONTENT_PARA2 =
+  "Our Gym is now open and also have live-streaming all our classes via the video chat app, Zoom. Our classes include Hatha Yoga, Zumba, Mat Pilates, Tabata, Les Mills BodyPump, and Les Mills BodyCombat. A combination of cardio, strength training, core, and stretch sequences that will transform your body. Our high-energy, effective workouts not only promises to build, tone, and strengthen the muscles, and we also promise visible results in just one week.";
+
 export const img_array = [CLOCK_URL, WORKOUT_URL, NUTRITION_URL];
 export const imgHeadingsArray = ["PROGRESSION", "WORKOUT", "NUTRITIONS"];
 export const imgDescriptionsArray = [
