bb8fa6bee50bf05592abc78a43f5ff92a4781c98
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-07-20 14:39:53 +0530

    updates undo

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
   return (
     <div>
       <Bg />
-      <div className="flex flex-col lg:flex-row items-center lg:items-start sticky top-[100px] lg:space-x-8">
-        <div className="lg:block hidden">
+      <div className="flex sticky top-[100px]">
+        <div className="yes">
           <motion.img
             initial={{ x: -100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
@@ -21,26 +21,26 @@ const Hero = () => {
               stiffness: 70,
             }}
             src={heroine}
-            className="w-[28rem] lg:w-full"
+            className="heroine w-[42rem]"
             alt="Heroine"
           />
         </div>
-        <div className="flex flex-col items-center lg:items-start mt-8 lg:mt-0">
+        <div className="flex flex-col items-center ml-16 arsenal-sc-regular">
           <motion.h1
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.2 }}
-            className="text-5xl lg:text-[5.5rem] tracking-wider text-center lg:text-left"
+            className="text-[5.5rem] mt-32 tracking-wider web-title"
           >
-            <span className="text-7xl lg:text-9xl">F</span>it
-            <span className="text-7xl lg:text-9xl">L</span>ife{" "}
-            <span className="text-7xl lg:text-9xl">G</span>ym
+            <span className="text-9xl">F</span>it
+            <span className="text-9xl">L</span>ife{" "}
+            <span className="text-9xl">G</span>ym
           </motion.h1>
           <motion.h3
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.4 }}
-            className="text-lg lg:text-2xl tracking-wide text-center lg:text-left mt-4 lg:mt-0"
+            className="text-2xl tracking-wide "
           >
             {TAGLINE}
           </motion.h3>
@@ -48,7 +48,7 @@ const Hero = () => {
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.6 }}
-            className="text-base lg:text-xl tracking-wide mt-4 lg:mt-10 max-w-xl text-center lg:text-left"
+            className="text-xl tracking-wide mt-10 max-w-xl text-center"
           >
             {PURPOSE}
           </motion.h3>
@@ -56,9 +56,8 @@ const Hero = () => {
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.75 }}
-            className="mt-6"
           >
-            <motion.button className="btn contact-hover rounded-full text-white tracking-wider p-3 pl-6 pr-6">
+            <motion.button className="btn contact-hover rounded-full text-white tracking-wider p-3 pl-6 pr-6 mt-6">
               Contact Us
             </motion.button>
           </motion.div>
