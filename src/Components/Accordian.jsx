7d20e755fe7fd9458dbb6217b750a20c7589ff11
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-07-21 01:55:17 +0530

    FAQ made responsive

--- src/Components/Accordian.jsx
+++ src/Components/Accordian.jsx
@@ -11,9 +11,9 @@ const Accordian = () => {
 
   return (
     <div className="bg-purple-50">
-      <div className="w-2/3 mx-auto select-none">
+      <div className="w-[97%] md:w-2/3 mx-auto select-none">
         <div className="mt-20 mb-10 text-purple-950">
-          <h1 className="text-6xl arsenal-sc-bold text-center mt-10">
+          <h1 className="text-4xl md:text-6xl arsenal-sc-bold text-center mt-8 md:mt-10">
             Frequently Asked Questions
           </h1>
         </div>
@@ -22,14 +22,14 @@ const Accordian = () => {
             <div key={i}>
               <div
                 onClick={() => handleAccordion(i)}
-                className="bg-purple-100 p-7 m-5 rounded-xl cursor-pointer"
+                className="bg-purple-100 p-4 my-6 mx-3 md:p-7 md:m-5 rounded-xl cursor-pointer"
               >
                 <div className="flex justify-between ">
-                  <h1 className="text-xl dm-sans-font ">{faq.Q}</h1>
+                  <h1 className="md:text-xl text-lg dm-sans-font w-[87%]">{faq.Q}</h1>
                   <button>{openIndex === i ? "⬆️" : "⬇️"}</button>
                 </div>
                 {openIndex === i ? (
-                  <p className="mt-5 text-gray-700 w-5/6">{faq.A}</p>
+                  <p className="mt-5 text-gray-700 w-[95%] md:w-5/6">{faq.A}</p>
                 ) : null}
 
                 {/* explanation with example
