644e6a0dfba5270df607ee854780b3604e321cea
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-07-20 22:33:22 +0530

    AboutGym made responsive

--- src/Components/AboutGym.jsx
+++ src/Components/AboutGym.jsx
@@ -10,9 +10,9 @@ import ServicesSection from "./ServicesSection";
 const AboutGym = () => {
   return (
     <div className="bg-purple-50">
-      <div className="flex mt-28 ">
+      <div className="flex flex-col md:flex-row mt-28 ">
         <img
-          className="w-[40%] rounded-3xl ml-40 mb-40 mr-14 hover:scale-105 transition duration-500 ease-in-out"
+          className=" md:ml-40 md:mr-14 mx-auto w-[85%] md:w-[40%] rounded-3xl mb-8 md:mb-40 hover:scale-105 transition duration-500 ease-in-out"
           src={ATHELETE_URL}
           alt=""
         />
@@ -20,17 +20,17 @@ const AboutGym = () => {
           initial={{ z: 100, opacity: 0 }}
           whileInView={{ z: 0, opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.25, ease: "easeIn" }}
-          className="w-[38%] flex flex-col justify-center content h-fit"
+          className="w-[80%] m-auto md:w-[38%] flex flex-col text-center md:text-left items-center md:items-start justify-center content h-fit"
         >
-          <h1 className="text-[3.5rem] tracking-wider text-purple-800 arsenal-sc-regular">
-            <span className="text-7xl">A</span>bout{" "}
-            <span className="text-7xl">O</span>ur{" "}
-            <span className="text-7xl">G</span>ym
+          <h1 className="text-4xl md:text-[3.5rem] pb-5 md:pb-3 tracking-wider text-purple-800 arsenal-sc-regular">
+            <span className="text-6xl md:text-7xl">A</span>bout{" "}
+            <span className="text-6xl md:text-7xl">O</span>ur{" "}
+            <span className="text-6xl md:text-7xl">G</span>ym
           </h1>
           <h2 className="mt-0 font-bold text-xl">{ABOUT_HEADING}</h2>
           <p className="mt-8 text-gray-500 text-lg">{ABOUT_CONTENT_PARA1}</p>
           <p className="mt-3 text-gray-500 text-lg">{ABOUT_CONTENT_PARA2}</p>
-          <button className="btn arsenal-sc-regular contact-hover w-2/6 rounded-full text-white tracking-wider p-3 pl-6 pr-6 mt-6">
+          <button className="btn arsenal-sc-regular contact-hover w-1/2 md:w-2/6 rounded-full text-white tracking-wider p-3 pl-6 pr-6 mt-6">
             Take a Tour &rarr;
           </button>
         </motion.div>
